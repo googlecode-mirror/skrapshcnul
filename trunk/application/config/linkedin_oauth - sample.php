6abbef06d7887d280ad2ec871252d563edae310a
stiucsib86@gmail.com <stiucsib86@gmail.com@843a4741-eabe-2654-631f-e5b33b1951e9>
2011-09-25 16:42:27 +0000

    + Somemore minor file changes to commit.

---
+++ trunk/application/config/linkedin_oauth - sample.php
@@ -0,0 +1,19 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * Name:  Lunchsparks Notifications Config
+ * 
+ * Author: @stiucsib86
+ * 
+ * Created:  20110911
+ */
+
+	/**
+	 * Tables.
+	 **/
+	$config['linkedin_appKey']		= '';
+	$config['linkedin_appSecret']	= '';
+	
+	$config['callbackUrl']	= '';
+	
+	
+?>
