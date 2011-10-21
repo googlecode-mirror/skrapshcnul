c02e4100a776a725b8df57e7f5f0d2f186aa5dc7
iamneit@gmail.com <iamneit@gmail.com@843a4741-eabe-2654-631f-e5b33b1951e9>
2011-10-21 09:21:57 +0000

    Same.

--- trunk/application/views/user/profile/my_ratings.php
+++ trunk/application/views/user/profile/my_ratings.php
@@ -1,7 +1,10 @@
 <div class="widget-box">
 	<h4 class="widget-title">My Ratings</h4>
 	<div id="user-ratings">
-		<div class="rating-">6.0</div>
+		<div class="rating-points">
+			<span><?php echo $profile_stats['rating'] ?></span>
+			<div style="margin-top:5px; font-weight:bold;font-size: small">reputation</div>
+		</div>
 	</div>
 	<div class="clearfix"></div>
 </div>
