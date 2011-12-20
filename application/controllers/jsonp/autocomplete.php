e219199a90773920a8494c03fe39790cba401570
stiucsib86 <stiucsib86@gmail.com>
2011-12-20 16:47:23 +0000

    + Autocomplete for admin/dashboard/recommendation.

--- application/controllers/jsonp/autocomplete.php
+++ application/controllers/jsonp/autocomplete.php
@@ -3,7 +3,7 @@
 /**
  * System Administrative Page
  */
-class People extends CI_Controller {
+class Autocomplete extends CI_Controller {
 
 	function __construct() {
 		parent::__construct();
@@ -33,31 +33,41 @@ class People extends CI_Controller {
 
 	}
 
-	public function _remap($method) {
-		if ($method == 'some_method_to_bypass') {
-			$this -> $method();
-		} else {
-			$this -> index();
-		}
+	function index() {
+
+		echo 'Welcome to Lunchsparks JSON Autocomplete';
+
 	}
 
-	function index() {
-		$userid = ($this -> uri -> segment(3));
-		if (!$userid || !is_numeric($userid)) {
+	function name() {
+
+		//$keywords = ($this -> uri -> segment(4));
+		$keywords = $this -> input -> get('keywords');
+		if (!$keywords) {
 			$error['domain'] = "global";
 			$error['reason'] = "invalidParameter";
-			$error['message'] = "Invalid value 'userId'. Values must match the following regular expression: 'me|[0-9]+'";
+			$error['message'] = "Invalid value 'keywords'.";
 			$error['locationType'] = "parameter";
-			$error['location'] = "userId";
+			$error['location'] = "keywords";
 			$this -> data['errors'] = $error;
 			return $this -> _json_prep();
 		}
-		
+
 		## TODO - Map to functions
-		$asso_array = ($this -> uri -> uri_to_assoc(4));
-		
-		$this -> results = $this -> ls_profile -> getPublicProfile($userid);
-		
+		$asso_array = ($this -> uri -> uri_to_assoc(5));
+
+		$this -> db -> select('*');
+		$this -> db -> from('lss_users_profile AS lup');
+		$this -> db -> like('alias', $keywords);
+		$this -> db -> or_like('firstname', $keywords);
+		$this -> db -> or_like('lastname', $keywords);
+		$this -> db -> join('lss_users_profile_social_links AS lupsl', 'lupsl.user_id = lup.user_id', 'left');
+		$query = $this -> db -> get();
+
+		if ($query -> num_rows() > 0) {
+			$this -> data['results'] = ($query -> result_array());
+		}
+
 		$this -> _json_prep();
 
 	}
@@ -76,13 +86,13 @@ class People extends CI_Controller {
 			}
 		} else {
 			$this -> json_result['completed_in'] = number_format(time() - $this -> start_time, 3, '.', '');
-			if (empty($this -> results)) {
-					$this -> json_result['results'] = FALSE;
-					$this -> json_result['reason'] = "No results";
-				} else {
-					$this -> json_result['results'][] = $this -> results;
-				}
-			
+			if (empty($this -> data['results'])) {
+				$this -> json_result['results'] = FALSE;
+				$this -> json_result['reason'] = "No results";
+			} else {
+				$this -> json_result['results'] = $this -> data['results'];
+			}
+
 			if ($this -> callback) {
 				print_r($this -> callback . '(' . json_encode($this -> json_result) . ')');
 			} else {
@@ -90,7 +100,5 @@ class People extends CI_Controller {
 			}
 		}
 	}
-
-	
 
 }
--- application/views/admin/recommendations.php
+++ application/views/admin/recommendations.php
@@ -52,19 +52,35 @@
 			</table>
 			<div style="background: #F8F8F8; border: 1px solid #CCCCCC; padding: 10px; margin: 10px 0;">
 				<div class="title" style="background: #F1F1F1; padding: 10px; cursor: pointer;" onclick="jQuery(this).next().toggle();">Create new recommendation.</div>
-				<div style="display:none;">
+				<div style="display:block;">
 					<form method="post">
 						<div>
-							<label>User ID</label>
-							<input type="text" />
+							<div style="display: inline-block; width: 300px; vertical-align: top;">
+								<label>User</label>
+								<input type="text" id="user_name" name="user_name" class="ls_user_autocomplete" placeholder="alias, firstname, lastname" />
+								<input type="hidden" id="f_h_user_id">
+							</div>
+							<div style="display: inline-block; width: 50px; padding-top:10px; vertical-align: bottom;">
+								<div class="ls-profile-hover" ls-data-userid="">
+									<!-- image placeholder -->
+								</div>
+							</div>
 						</div>
 						<div>
-							<label>Target User ID</label>
-							<input type="text" />
+							<div style="display: inline-block; width: 300px; vertical-align: top;">
+								<label>Target User</label>
+								<input type="text" id="target_user_name" name="target_user_name" class="ls_user_autocomplete" placeholder="alias, firstname, lastname" />
+								<input type="hidden" id="f_h_target_user_id">
+							</div>
+							<div style="display: inline-block; width: 50px; padding-top:10px; vertical-align: bottom;">
+								<div class="ls-profile-hover" ls-data-userid="">
+									<!-- image placeholder -->
+								</div>
+							</div>
 						</div>
 						<div>
 							<label>Reason</label>
-							<input type="text" />
+							<input type="text" id="reason" name="reason" placeholder="Reason" />
 						</div>
 						<input type="submit" />
 					</form>
@@ -122,3 +138,55 @@
 		</div>
 	</div>
 </div>
+
+<script>
+jQuery('.ls_user_autocomplete').each( function() {
+	var el = jQuery(this);
+	el.autocomplete({
+		source: function( request, response ) {
+			var keywords = el.val();
+			jQuery.getJSON("/jsonp/autocomplete/name?callback=?",
+			{ 
+				keywords: keywords,
+			}, function(data) {
+				//console.log(data);
+				response( jQuery.map( data.results, function( item ) {
+					return {
+						label: item.firstname + (item.lastname ? ", " + item.lastname : ""),
+						value: item.firstname + (item.lastname ? ", " + item.lastname : ""),
+						user_id: item.user_id,
+						profile_img: item.profile_img,
+						ls_pub_url : item.lunchsparks,
+						firstname : item.firstname
+					}
+				}));
+			});
+		},
+		minLength: 2,
+		select: function( event, ui ) {
+			console.log( ui.item ?
+				"Selected: " + ui.item.label :
+				"Nothing selected, input was " + this.value);
+			el.next().val(ui.item.user_id);
+			el.parent().next().children().attr('ls-data-userid', ui.item.user_id);
+			el.parent().next().children().html(
+				'<div class="profile-img-45 hover-profile-card" ls:uid="'+ui.item.user_id+'">'+
+					'<a href="'+ui.item.ls_pub_url+'">'+
+						'<img title="'+ui.item.firstname+'" src="'+ui.item.profile_img+'">'+
+					'</a>'+
+				'</div>'
+			);
+			
+		},
+		open: function() {
+			jQuery( this ).removeClass( "ui-corner-all" ).addClass( "ui-corner-top" );
+		},
+		close: function() {
+			jQuery( this ).removeClass( "ui-corner-top" ).addClass( "ui-corner-all" );
+		}
+	});
+});
+</script>
+<script>
+	// TODO Form submit handler
+</script>
