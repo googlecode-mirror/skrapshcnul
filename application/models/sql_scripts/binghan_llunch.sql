d6df4b1853fba61ec7d54e8f6bd70b84404f55a6
stiucsib86 <stiucsib86@gmail.com>
2012-02-18 02:47:10 +0000

    DB Structure updated.

--- application/models/sql_scripts/binghan_llunch.sql
+++ application/models/sql_scripts/binghan_llunch.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 16, 2012 at 04:39 PM
+-- Generation Time: Feb 18, 2012 at 02:39 AM
 -- Server version: 5.1.53
 -- PHP Version: 5.3.8
 
@@ -182,6 +182,8 @@ CREATE TABLE IF NOT EXISTS `lss_places` (
   `place_id` int(11) NOT NULL AUTO_INCREMENT,
   `name` text NOT NULL,
   `logo` varchar(255) NOT NULL,
+  `primary_phone` varchar(250) NOT NULL,
+  `url` varchar(250) NOT NULL,
   `location` text NOT NULL,
   `geo_lat` varchar(11) NOT NULL,
   `geo_long` varchar(11) NOT NULL,
@@ -189,7 +191,7 @@ CREATE TABLE IF NOT EXISTS `lss_places` (
   `created_on` datetime NOT NULL,
   `updated_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`place_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 -- --------------------------------------------------------
 
@@ -202,6 +204,23 @@ CREATE TABLE IF NOT EXISTS `lss_places_verification_status` (
   `status` tinyint(1) NOT NULL,
   `remarks` text NOT NULL,
   `updated_by` int(11) NOT NULL,
+  `created_on` datetime NOT NULL,
+  `updated_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`place_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lss_places_xtra_restaurant`
+--
+
+CREATE TABLE IF NOT EXISTS `lss_places_xtra_restaurant` (
+  `place_id` int(11) NOT NULL,
+  `cuisine` varchar(250) NOT NULL,
+  `opening_hours` varchar(250) NOT NULL,
+  `special_features` text NOT NULL,
+  `extras` text NOT NULL,
   `created_on` datetime NOT NULL,
   `updated_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`place_id`)
@@ -356,7 +375,7 @@ CREATE TABLE IF NOT EXISTS `lss_users_login_history` (
   `user_agent` varchar(100) NOT NULL,
   `createdOn` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=205 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=206 ;
 
 -- --------------------------------------------------------
 
