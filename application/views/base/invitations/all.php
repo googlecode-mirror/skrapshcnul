266a822756d6055e9a2606f5401ad651af27e097
kusum18 <kusum18@gmail.com>
2012-02-18 13:10:35 +0000

    Under invitations page : Case if user has zero invitations and he has not yet sent any invitations yet then he shouldnt be shown the Invitations status.Discussed with Tien

--- application/views/base/invitations/all.php
+++ application/views/base/invitations/all.php
@@ -2,12 +2,12 @@
 	<div class="c-pages shadow-rounded">
 		<h2><?php echo $tpl_page_title; ?></h2>
 		<h3 class="sub-heading">Although we are still in closed Alpha stage, we are giving out invitations to you so that you can invite those who you think will benefit from this platform</h3>
-		
+		<?php if(!($invitation['invitation_left']==0 && empty($invite_logs))) {?>
 		<div class="section">
 			<?php $this -> load -> view('base/invitations/includes/status');?>
 		</div>
 		<div class="clearfix">&nbsp;</div>
-		
+		<?php } ?>
 		<div class="section">
 			<?php $this -> load -> view('base/invitations/includes/invite');?>
 		</div>
