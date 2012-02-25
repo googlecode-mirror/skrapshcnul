69c26d5b967e785fa2fcb52eb239d98dade888fc
kusum18@gmail.com <kusum18@gmail.com@843a4741-eabe-2654-631f-e5b33b1951e9>
2012-02-25 13:41:34 +0000

    Cronjob php script for notifications email

---
+++ trunk/application/controllers/cronjob/notification.php
@@ -0,0 +1,52 @@
+<?php  
+
+
+class Notification extends CI_Controller {
+
+	function __construct() {
+		parent::__construct();
+		
+		$this -> load -> library('ls_notifications');
+		$this->load->library('email');
+		
+		$this->config->load('cronjob_notification');
+		
+		$this -> load -> model('ls_notifications_model');
+	}
+
+	function index() {
+		
+	}
+	
+	function sample()
+	{
+		$frequency = $this->config->item('cronjob_frequency');
+		$sender = $this->config->item('cronjob_sender');
+		$last_time = time() - ($frequency * 60);
+		//Step 1: Collect all notifications
+		$temp = $this -> ls_notifications_model -> get_notifications_cronjob($last_time);
+		if($temp)
+		{
+			// Send out all emails
+			foreach ($temp as $row) {
+				echo $row['email']." ".$row['message'];
+				$this -> send_email($sender,$row['email'],$row['message']);
+			}
+			
+			
+		}
+		else {
+			// All emails have already been dispatched
+		}
+	}
+	
+	function send_email($from,$to,$message)
+	{
+		$this->email->from($from, 'Lunchsparks');
+		$this->email->to($to);
+		$this->email->subject('Lunchsparks notifications');
+		$this->email->message($message);
+		$this->email->send();
+	}
+}
+?>
